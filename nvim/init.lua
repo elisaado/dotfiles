671a86da271f1be0c5e09d00c766d87383ec55e0
Eli Saado <eli5saado@gmail.com>
2024-10-05 23:52:58 +0200

    resume to last pos

--- nvim/init.lua
+++ nvim/init.lua
@@ -970,6 +970,19 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
+-- resume line in file
+-- from https://github.com/creativenull/dotfiles/blob/9ae60de4f926436d5682406a5b801a3768bbc765/config/nvim/init.lua#L70-L86
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function(args)
+    local valid_line = vim.fn.line [['"]] >= 1 and vim.fn.line [['"]] < vim.fn.line '$'
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+    if valid_line and not_commit then
+      vim.cmd [[normal! g`"]]
+    end
+  end,
+})
+
 -- empty setup using defaults
 
 -- The line beneath this is called `modeline`. See `:help modeline`
